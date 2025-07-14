7d85c5bee398a2a2bb589e19535ad575831faf14
Sergey Zelenov <github@zelenov.su>
2025-07-14 19:10:46 +0200

    openai integration, v1

--- src/bot.ts
+++ src/bot.ts
@@ -1,13 +1,22 @@
 import "dotenv/config";
 import { Bot } from "grammy";
+import { getAnswer } from "./openai";
 
 const bot = new Bot(process.env.BOT_TOKEN as string);
 
 bot.command("start", (ctx) => {
   ctx.reply("Welcome! Up and running.");
 });
-bot.on("message", (ctx) => {
-  ctx.reply("Got another message!");
+bot.on("message", async (ctx) => {
+  if (!ctx.message.text) {
+    ctx.reply("Пожалуйста, пришлите текст.");
+    return;
+  }
+
+  ctx.replyWithChatAction("typing");
+
+  const response = await getAnswer(ctx.message.text);
+  ctx.reply(response);
 });
 
 export default bot;
