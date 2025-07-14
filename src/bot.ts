70d56bf1173a0a2c12d6512d19eef6601dedc7d7
Sergey Zelenov <github@zelenov.su>
2025-07-14 19:53:20 +0200

    handle photos

--- src/bot.ts
+++ src/bot.ts
@@ -1,21 +1,40 @@
 import "dotenv/config";
 import { Bot } from "grammy";
 import { getAnswer } from "./openai";
+import { ResponseInputMessageContentList } from "openai/resources/responses/responses";
 
-const bot = new Bot(process.env.BOT_TOKEN as string);
+const BOT_TOKEN = process.env.BOT_TOKEN as string;
+
+const bot = new Bot(BOT_TOKEN);
 
 bot.command("start", (ctx) => {
   ctx.reply("Welcome! Up and running.");
 });
 bot.on("message", async (ctx) => {
-  if (!ctx.message.text) {
+  if (!ctx.message.text && !ctx.message.caption) {
     ctx.reply("Пожалуйста, пришлите текст.");
     return;
   }
 
   ctx.replyWithChatAction("typing");
 
-  const response = await getAnswer(ctx.message.text);
+  const input: ResponseInputMessageContentList = [
+    {
+      type: "input_text",
+      text: (ctx.message.text || ctx.message.caption) as string,
+    },
+  ];
+
+  if (ctx.message.photo) {
+    const file = await ctx.getFile();
+    input.push({
+      type: "input_image",
+      detail: "low",
+      image_url: `https://api.telegram.org/file/bot${BOT_TOKEN}/${file.file_path}`,
+    });
+  }
+
+  const response = await getAnswer(input);
   ctx.reply(response);
 });
 
